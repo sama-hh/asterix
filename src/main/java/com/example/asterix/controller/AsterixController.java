d1eaca22e94b63a9476413b84a69d5164cebcd2b
sama-hh <samahajimuradovah@gmail.com>
2024-09-23 12:02:18 +0200

    crud

--- src/main/java/com/example/asterix/controller/AsterixController.java
+++ src/main/java/com/example/asterix/controller/AsterixController.java
@@ -1,8 +1,7 @@
 package com.example.asterix.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 import com.example.asterix.repository.CharacterRepository;
 import com.example.asterix.model.Character;
 
@@ -10,17 +9,35 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/asterix")
+@RequiredArgsConstructor
 public class AsterixController {
     private final CharacterRepository characterRepository;
-
-    public AsterixController(CharacterRepository characterRepository) {
-        this.characterRepository = characterRepository;
-    }
 
     @GetMapping("/characters")
     public List<Character> findAll() {
         List<Character> people = characterRepository.findAll();
         return people;
+    }
+
+    @PostMapping("characters")
+    public Character save(@RequestBody Character character) {
+        Character saved = characterRepository.save(character);
+        return saved;
+    }
+
+    @DeleteMapping("characters/{id}")
+    public void delete(@PathVariable String id) {
+        characterRepository.deleteById(id);
+    }
+
+    @PutMapping("characters/{id}")
+    public Character update(@PathVariable String id, @RequestBody Character character) {
+        Character existingCharacter = characterRepository.findById(id)
+                .orElse(null);
+        characterRepository.deleteById(id);
+        Character newCharacter = character.withId(id);
+        characterRepository.save(newCharacter);
+        return newCharacter;
     }
 
 }
--- src/main/java/com/example/asterix/model/Character.java
+++ src/main/java/com/example/asterix/model/Character.java
@@ -1,7 +1,9 @@
 package com.example.asterix.model;
 
+import lombok.With;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("character")
+@With
 public record Character(String id, String name, int age, String profession) {
 }
